92a37f8003a2f4c34688b53e356319907c88d1bf
@vcastro-dev <vcastro2395@gmail.com>
2024-02-26 02:35:53 -0300

    feat: layout inicial página de produtos

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="flex items-center justify-between w-full p-4">
+      <h1 className="text-2xl font-bold">Header</h1>
+    </header>
+  );
+}
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,24 @@
+import { Container } from "../Container/Container";
+
+export function Nav() {
+  return (
+    <nav className="flex items-center justify-between w-full p-4">
+      <Container>
+        <ul className="flex">
+          <li className="mx-2">
+            <a href="/">Home</a>
+          </li>
+          <li className="mx-2">
+            <a href="/products">Products</a>
+          </li>
+          <li className="mx-2">
+            <a href="/about">About</a>
+          </li>
+          <li className="mx-2">
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </Container>
+    </nav>
+  );
+}
