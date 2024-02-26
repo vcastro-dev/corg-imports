a4b0c88b96c70acd5aa502faa5165777208e8d67
@vcastro-dev <vcastro2395@gmail.com>
2024-02-26 03:04:13 -0300

    feat: primeira versão da home

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,7 +3,29 @@ import { Container } from "../ui/Container/Container";
 export function Home() {
   return (
     <Container>
-      <h1 className="text-4xl font-bold text-center">Home</h1>
+      <>
+        <h1 className="text-4xl font-bold text-center">A Barkbusters</h1>
+        <p className="text-lg text-center mt-4">
+          A marca de protetores de mão BARKBUSTERS® foi inicialmente fabricada
+          na Austrália em 1984. Nosso entusiasmo pela pilotagem de motocicletas
+          tem contribuído para o desenvolvimento dos melhores protetores de mão
+          disponíveis no mercado hoje. BARKBUSTERS® é uma marca registrada da
+          Rideworx Pty Ltd e é ativamente promovida em todo o mundo. A Rideworx
+          é dedicada à melhoria contínua e ao desenvolvimento constante de sua
+          linha de produtos, fabricando com os mais altos padrões possíveis. O
+          design excepcional e a qualidade dos produtos fabricados pela Rideworx
+          são amplamente reconhecidos, a ponto de produzirem protetores de mão
+          para clientes OEM tanto na Austrália quanto internacionalmente. Na
+          concepção de todos os produtos BARKBUSTERS®, a Rideworx compromete-se
+          a incorporar estilo, resistência, durabilidade e facilidade de
+          instalação. Esses elementos fundamentais são a base do compromisso da
+          Rideworx em oferecer produtos que não apenas atendam, mas superem as
+          expectativas dos motociclistas em todo o mundo.
+        </p>
+      </>
+      <>
+        <h1 className="text-4xl font-bold text-center mt-8">Nossos Produtos</h1>
+      </>
     </Container>
   );
 }
