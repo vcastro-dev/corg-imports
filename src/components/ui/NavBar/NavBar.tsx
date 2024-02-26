d5fa8f1f271909f54e0d7e877c7a34f579072017
@vcastro-dev <vcastro2395@gmail.com>
2024-02-26 03:33:48 -0300

    style: aplicando hover nas opções do header

--- src/components/ui/NavBar/NavBar.tsx
+++ src/components/ui/NavBar/NavBar.tsx
@@ -1,20 +1,31 @@
+import Link from "next/link";
 import { Container } from "../Container/Container";
 
 export function NavBar() {
   return (
     <nav className="flex items-center justify-between w-full p-4">
       <Container>
-        <ul className="flex">
-          <li className="mx-2">
-            <a href="/">Home</a>
-          </li>
-          <li className="mx-2">
-            <a href="/products">Produtos</a>
-          </li>
-          <li className="mx-2">
-            <a href="/contact">Contato</a>
-          </li>
-        </ul>
+        <div className="flex">
+          <Link
+            className="mx-2 hover:bg-gray-200 rounded-md px-3 py-2"
+            href="/"
+          >
+            Home
+          </Link>
+          <Link
+            className="mx-2 hover:bg-gray-200 rounded-md px-3 py-2"
+            href="/products"
+          >
+            Produtos
+          </Link>
+          <Link
+            className="mx-2 hover:bg-gray-200 rounded-md px-3 py-2"
+            href="/contact"
+            passHref
+          >
+            Contato
+          </Link>
+        </div>
       </Container>
     </nav>
   );
