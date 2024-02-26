0419d9c06f24d15020e8f2a4fd4049feaea09a0d
@vcastro-dev <vcastro2395@gmail.com>
2024-02-26 03:26:23 -0300

    style: aplicando truncate no título do produto

--- src/components/pages/GalleryProducts/CardProduct.tsx
+++ src/components/pages/GalleryProducts/CardProduct.tsx
@@ -11,12 +11,12 @@ type Props = {
 export function CardProduct({ image, title, description, id }: Props) {
   return (
     <Card>
-      <div className="flex flex-col items-center">
-        <img src={image} alt={title} className="w-64 h-64 object-cover " />
-        <h2 className="text-2xl font-bold">{title}</h2>
-        <p className="text-lg text-center">{description}</p>
-        <Link href={`/products/${id}`} passHref>
-          <button className="bg-blue-500 text-white rounded-lg p-2 mt-2">
+      <div className="flex flex-col items-center p-4 gap-2 h-full">
+        <img src={image} alt={title} className="w-64 h-64 object-cover" />
+        <h2 className="text-2xl font-bold truncate">{title}</h2>
+        <p className="text-lg text-center truncate">{description}</p>
+        <Link href={`/products/${id}`} passHref className="w-full">
+          <button className="w-full mt-4 bg-blue-500 text-white px-4 py-2 rounded-md">
             Ver descrição
           </button>
         </Link>
